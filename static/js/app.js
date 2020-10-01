9fef0a5ddacfa2a40a5f07147cf98de4525df696
Seidi A Mohammad <seidiali21@yahoo.com>
2020-10-01 10:20:32 -0400

    added second

---
+++ static/js/app.js
@@ -0,0 +1,156 @@
+var select = d3.select('select');
+var panel = d3.select('.panel-body');
+
+d3.json('samples.json').then(data => {
+    // console.log(data.metadata[0]);
+
+    var names = data.names;
+
+    names.forEach(name => {
+        select.append('option').text(name).property('value',name);
+    });
+
+    showDemo(names[0]);
+});
+
+function showDemo(name) {
+    d3.json('samples.json').then(data => {
+        panel.html('');
+
+        var metadata = data.metadata.filter(obj => obj.id == name)[0];
+        console.log(metadata);
+        Object.entries(metadata).forEach(([key,value]) => {
+            panel.append('h6').text(`${key.toUpperCase()}: ${value}`)
+        });
+
+        
+    });
+}
+
+// create the function that gets the data and creates the plots for the id 
+function getPlot(id) {
+    
+    // get the data from the json file
+    d3.json("samples.json").then((data)=> {
+        console.log(data)
+
+        var wfreq = data.metadata.map(d => d.wfreq)
+        console.log(`Washing Freq: ${wfreq}`)
+
+        // filter sample values by id 
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
+
+        console.log(samples);
+
+        // get only top 10 sample values to plot and reverse for the plotly
+        var sampleValues = samples.sample_values.slice(0, 10).reverse();
+
+        // get only top 10 otu ids for the plot
+        var idValues = (samples.otu_ids.slice(0, 10)).reverse();
+        
+        // get the otu id's to the desired form for the plot
+        var idOtu = idValues.map(d => "OTU " + d)
+
+        console.log(`OTU IDS: ${idOtu}`)
+
+        // get the top 10 labels for the plot
+        var labels = samples.otu_labels.slice(0, 10);
+
+        console.log(`Sample Values: ${sampleValues}`)
+        console.log(`Id Values: ${idValues}`)
+
+        
+        // create trace variable for the plot
+        var trace = {
+            x: sampleValues,
+            y: idOtu,
+            text: labels,
+            type:"bar",
+            orientation: "h",
+        };
+
+        // create data variable
+        var data = [trace];
+
+        // create layout variable to set plots layout
+        var layout = {
+            title: "Top 10 OTU",
+            yaxis:{
+                tickmode:"linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 30,
+                b: 20
+            }
+        };
+
+        // create the bar plot
+        Plotly.newPlot("bar", data, layout);
+
+        //console.log(`ID: ${samples.otu_ids}`)
+        
+        // create the trace for the bubble chart
+        var trace1 = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+
+        };
+
+        // set the layout for the bubble plot
+        var layout = {
+            xaxis:{title: "OTU ID"},
+            height: 600,
+            width: 1300
+        };
+
+        // create the data variable 
+        var data1 = [trace1];
+
+        // create the bubble plot
+        Plotly.newPlot("bubble", data1, layout); 
+
+        // create pie chart
+        var tracePie = {
+            labels: idOtu,
+            values:sampleValues,
+            type:"pie",
+        }
+
+        var data = [tracePie]
+        
+        
+        Plotly.newPlot("gauge", data)
+
+    });    
+}
+
+
+// create the function for the initial data rendering
+function init() {
+    // select dropdown menu 
+    var dropdown = d3.select("#selDataset");
+
+    // read the data 
+    d3.json("samples.json").then((data)=> {
+        console.log(data)
+
+        // get the id data to the dropdwown menu
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
+
+        // call the functions to display the data and the plots to the page
+        getPlot(data.names[0]);
+        getInfo(data.names[0]);
+    });
+}
+
+init();
